5b30925d4502359d2921e7f1278f58a4897a7b78
syms-acessorios <syms.acessorios@gmail.com>
2025-08-11 23:25:45 -0300

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-window.IP_SERVER = "http://localhost:21465/api/";
-window.IP_SOCKET_IO = "http://localhost:21465/";
+window.IP_SERVER = "https://server-hf2t.onrender.com/api/";
+window.IP_SOCKET_IO = "https://server-hf2t.onrender.com/";
